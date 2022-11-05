c5ebbbd307c4814a7e6a14444c60e67481328303
Marvelous Ejiogu <ejiogumarvelous@gmail.com>
2022-11-05 14:58:34 +0100

    Counter App using custom hook and use reducer.

--- src/App.js
+++ src/App.js
@@ -7,19 +7,32 @@ import PageNotFound from "./pages/PageNotFound";
 import  Reducer  from "./pages/Reducer";
 import './index.css';
 
-function App() {
+function App () {
+  const [click, setClick] = useState(false);
+  const handleIconClick = () => {
+    if (click){
+      setClick(false)
+    }  else{setClick(true)};
+      
+  }
+  
   return (
     <Router>
       <nav>
         <div>
           <ul className="navbar">
-            <li>
+            <li className="nav-links">
         <Link to="/">CustomHook</Link>
         <Link to="/reducer">Reducer</Link>
         {/* <Link to="/testPage">TestPage</Link> */}
         <Link to="/errorBoundary">ErrorBoundary</Link>
             </li>
           </ul>
+        </div>
+
+        <div id="mobile-view" onClick={handleIconClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+
         </div>
       </nav>
     
